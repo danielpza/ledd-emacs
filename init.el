ae86140ba8ee4ddf78e39d53e576471ce2297c8a
Luis Perez <luisenrike@leddgroup.com>
2020-02-13 14:49:23 -0500

    Update init.el

--- init.el
+++ init.el
@@ -17,14 +17,13 @@
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+;; use-package
+(setq use-package-always-ensure t)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
 (eval-when-compile
   (require 'use-package))
-
-(setq use-package-always-ensure t)
 
 ;; core-packages:
 ;; evil
