65fc618e1402eb888959435401201dd351a4a09f
Luis Perez <luis.perez@kin.com>
2020-02-13 19:30:18 -0500

    magit and more refresh-packages

--- init.el
+++ init.el
@@ -1,9 +1,18 @@
 ;; functions
+
 (defun open-scratch-buffer ()
   (interactive)
   (switch-to-buffer (get-buffer-create "*scratch*")))
 
+(defun refresh-packages ()
+  (interactive)
+  (package-refresh-contents)
+  (package-install-selected-packages)
+  (package-autoremove))
+
+
 ;; defaults
+
 (fset 'yes-or-no-p 'y-or-n-p)
 (display-line-numbers-mode)
 
@@ -11,25 +20,38 @@
 (tool-bar-mode -1)
 
 ;; package.el
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+	     '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(setq package-selected-packages '(;; core
+				  use-package
+				  evil
+				  ivy
+				  org
+				  magit
+				  projectile
+				  general
+
+				  ;; ui
+				  doom-themes
+
+				  ;; finances
+				  ledger-mode))
+
 ;; use-package
-(setq use-package-always-ensure t)
+
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
 (eval-when-compile
   (require 'use-package))
 
-;; core-packages:
-;; evil
-;; ivy
-;; general
-;; use-package
+(setq use-package-compute-statistics t)
 
 (use-package evil
   :config
@@ -38,8 +60,18 @@
 (use-package ivy
   :config
   (setq ivy-use-virtual-buffers t
-        ivy-height 20)
+	ivy-height 20)
   (ivy-mode 1))
+
+(use-package projectile
+  :config
+  (projectile-mode 1)
+  (setq projectile-completion-system 'ivy))
+
+(use-package magit
+  :commands magit-status
+  :config
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
 
 (use-package general
   :config
@@ -53,34 +85,35 @@
     :prefix my-local-leader)
 
   (my-leader-def 'normal
-                 "h" help-map
-                 "w" evil-window-map)
+    "h" help-map
+    "p" projectile-command-map
+    "w" evil-window-map)
 
   (my-leader-def 'normal
-                 ":" #'execute-extended-command)
+    ":" #'execute-extended-command)
 
   (my-leader-def 'visual
-                 ";" #'comment-dwim)
+    ";" #'comment-dwim)
 
   (my-leader-def 'normal
-                 :infix "f"
-                 "f" #'find-file)
+    :infix "f"
+    "f" #'find-file
+    "s" #'save-buffer)
 
   (my-leader-def 'normal
-                 :infix "b"
-                 "b" #'ivy-switch-buffer
-                 "p" #'previous-buffer
-                 "n" #'next-buffer
-                 "s" #'open-scratch-buffer))
+    :infix "g"
+    "g" #'magit-status)
 
-;; not-core
+  (my-leader-def 'normal
+    :infix "b"
+    "b" #'ivy-switch-buffer
+    "p" #'previous-buffer
+    "n" #'next-buffer
+    "s" #'open-scratch-buffer))
 
-;; finance
 (use-package ledger-mode
-  :defer t)
+  :mode "\\.ledger\\'")
 
-;; ui
 (use-package doom-themes
-  :defer t
-  :init
+  :config
   (load-theme 'doom-dark+ t))
