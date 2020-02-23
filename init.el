6295c79a5d1db108929c4adacd4d8fc7243f28d4
Daniel Perez Alvarez <danielpza@protonmail.com>
2020-02-23 17:48:20 -0500

    spc f R rename-file

--- init.el
+++ init.el
@@ -39,6 +39,23 @@
   (interactive)
   (call-interactively (if (eq major-mode 'eshell-mode) 'bury-buffer 'projectile-run-eshell)))
 
+;; https://stackoverflow.com/a/384346
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
+
 ;; helpers
 
 ;; from doom
@@ -301,6 +318,7 @@
     "f t" #'treemacs
     "f p" #'find-init-file
     "f r" #'counsel-recentf
+    "f R" #'rename-file-and-buffer
     "f f" #'counsel-find-file
     "f d" #'delete-current-file
     "f s" #'save-buffer
